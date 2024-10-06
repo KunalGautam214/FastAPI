d11af80bb6bc0d96b60eb8eb8e0ca0d951c04abb
KunalGautam214 <kunalgautam214@gmail.com>
2024-10-06 20:00:18 +0530

    Path params validations

--- path_params_validation.py
+++ path_params_validation.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Path
 from typing import Annotated
 
-
 app = FastAPI()
 
 
 @app.get('/items/{item_id}')
 async def items(item_id: Annotated[int | None, None] = None):
     return {'item_id': item_id}
+
 
 @app.get('/items/validation/{item_id}')
 async def items(item_id: Annotated[int | None, Path(gt=5, lt=15)]):
