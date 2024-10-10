9b9ef036cd375b062488d0fb08e10917c2cede1b
KunalGautam214 <kunalgautam214@gmail.com>
2024-10-10 16:23:38 +0530

    Form data

---
+++ form_data.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Form
+from pydantic import BaseModel
+
+from typing import Annotated
+
+app = FastAPI()
+
+
+class UserDataForm(BaseModel):
+    username: str
+    password: str
+
+
+@app.post('/user')
+async def create_user(username: Annotated[str, Form()],
+                      passowrd: Annotated[str, Form()]):
+    return {'username': username}
+
+
+@app.post('/user/form-data')
+async def create_user(user_data: Annotated[UserDataForm, Form()]):
+    return {'username': user_data}
